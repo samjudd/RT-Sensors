afdea350145c32a0eb34fd2d02e92c0a85bfa939
Johannes Norheim <norheim@mit.edu>
2014-10-13 17:08:36 -0400

    Adding websocket interaction in order to build Sensor Web GUI

--- liveGraph.py
+++ liveGraph.py
@@ -1,4 +1,5 @@
 import matplotlib
+
 matplotlib.use('TKAgg')
 
 import os
@@ -11,189 +12,185 @@ import u3
 
 
 class Graph:
-	'''Class for displaying the 3 subplots in real time for the hot fire test
+    '''Class for displaying the 3 subplots in real time for the hot fire test
 	The plots are: Pressure, Gas concentrations, and force'''
 
-	def __init__(self):
-		self.interval = 30 #milliseconds per sample
-		self.length = 100 #saved points
-		self.t = [-float(self.interval)/1000 * k for k in range(self.length, 0, -1)]
+    def __init__(self):
+        self.interval = 30  # milliseconds per sample
+        self.length = 100  # saved points
+        self.t = [-float(self.interval) / 1000 * k for k in range(self.length, 0, -1)]
 
+        self.pressure1 = [0] * self.length
+        # self.pressure2 = [0] * self.length
+        #
+        # self.gas1 = [0] * self.length
+        # self.gas2 = [0] * self.length
+        # self.gas3 = [0] * self.length
 
-		self.pressure1 = [0] * self.length
-		# self.pressure2 = [0] * self.length
-		#
-		# self.gas1 = [0] * self.length
-		# self.gas2 = [0] * self.length
-		# self.gas3 = [0] * self.length
+        self.force1 = [0] * self.length
+        self.force2 = [0] * self.length
+        self.force3 = [0] * self.length
 
-		self.force1 = [0] * self.length
-		self.force2 = [0] * self.length
-		self.force3 = [0] * self.length
+        # Creates the "logs" folder if it doesn't exist
+        if not os.path.exists('logs'):
+            os.makedirs('logs')
 
-		# Creates the "logs" folder if it doesn't exist
-		if not os.path.exists('logs'):
-    		  os.makedirs('logs')
+        # creates a file in "logs" folder, with format "Day-Month-Year__Hour-Minute-Second"
+        # eg. 10/10/2014 12:51:30 AM is "10-10-2014__00-51-30"
+        self.log = open('logs/%s.txt' % time.strftime("%d-%m-%Y__%H-%M-%S"), 'wb')
+        self.time = time.time()
 
-	  	# creates a file in "logs" folder, with format "Day-Month-Year__Hour-Minute-Second"
-	  	# eg. 10/10/2014 12:51:30 AM is "10-10-2014__00-51-30"
-		self.log = open('logs/%s.txt' % time.strftime("%d-%m-%Y__%H-%M-%S"), 'wb')
-		self.time = time.time()
+        self.d = u3.U3()
+        self.d.configU3()
+        self.d.getCalibrationData()
+        self.d.configIO(FIOAnalog=255)
 
+        # self.fig, axes = plt.subplots(nrows=3, ncols=1)
+        # (self.ax1, self.ax2, self.ax3) = axes
+        self.fig, (self.ax1, self.ax3) = plt.subplots(nrows=2, ncols=1)
 
-		self.d = u3.U3()
-		self.d.configU3()
-		self.d.getCalibrationData()
-		self.d.configIO(FIOAnalog=255)
+        plt.subplots_adjust(bottom=0.05, top=0.95, left=0.05, right=0.9)
 
-		# self.fig, axes = plt.subplots(nrows=3, ncols=1)
-		# (self.ax1, self.ax2, self.ax3) = axes
-		self.fig, (self.ax1, self.ax3) = plt.subplots(nrows=2, ncols=1)
+        self.ax1.set_ylabel('Pressure (psi)')
+        # self.ax2.set_ylabel('Concentration (ppm)')
+        self.ax3.set_ylabel('Force (N)')
 
-		plt.subplots_adjust(bottom=0.05, top=0.95, left=0.05, right=0.9)
+        self.ax1.set_ylim([0, 5])
+        # self.ax2.set_ylim([0,5])
+        self.ax3.set_ylim([-5, 5])
 
+        y = [0] * self.length
 
-		self.ax1.set_ylabel('Pressure (psi)')
-		# self.ax2.set_ylabel('Concentration (ppm)')
-		self.ax3.set_ylabel('Force (N)')
+        self.pressureLine1 = self.ax1.plot(self.t, y, label="Pressure 1")[0]
+        # self.pressureLine2 = self.ax1.plot(self.t, y, label="Pressure 1")[0]
+        #
+        # self.gasLine1 = self.ax2.plot(self.t, y, label="Gas 1")[0]
+        # self.gasLine2 = self.ax2.plot(self.t, y, label="Gas 2")[0]
+        # self.gasLine3 = self.ax2.plot(self.t, y, label="Gas 3")[0]
 
+        self.forceLine1 = self.ax3.plot(self.t, y, label="Force 1")[0]
+        self.forceLine2 = self.ax3.plot(self.t, y, label="Force 2")[0]
+        self.forceLine3 = self.ax3.plot(self.t, y, label="Force 3")[0]
 
-		self.ax1.set_ylim([0,5])
-		# self.ax2.set_ylim([0,5])
-		self.ax3.set_ylim([-5,5])
+        self.ax1.legend(bbox_to_anchor=(1.0, 0.5), loc='center left', prop={'size': 10})
+        # self.ax2.legend(bbox_to_anchor = (1.0, 0.5), loc='center left', prop={'size':10})
+        self.ax3.legend(bbox_to_anchor=(1.0, 0.5), loc='center left', prop={'size': 10})
 
-		y = [0] * self.length
-
-		self.pressureLine1 = self.ax1.plot(self.t, y, label="Pressure 1")[0]
-		# self.pressureLine2 = self.ax1.plot(self.t, y, label="Pressure 1")[0]
-		#
-		# self.gasLine1 = self.ax2.plot(self.t, y, label="Gas 1")[0]
-		# self.gasLine2 = self.ax2.plot(self.t, y, label="Gas 2")[0]
-		# self.gasLine3 = self.ax2.plot(self.t, y, label="Gas 3")[0]
-
-		self.forceLine1 = self.ax3.plot(self.t, y, label="Force 1")[0]
-		self.forceLine2 = self.ax3.plot(self.t, y, label="Force 2")[0]
-		self.forceLine3 = self.ax3.plot(self.t, y, label="Force 3")[0]
-
-		self.ax1.legend(bbox_to_anchor = (1.0, 0.5), loc='center left', prop={'size':10})
-		# self.ax2.legend(bbox_to_anchor = (1.0, 0.5), loc='center left', prop={'size':10})
-		self.ax3.legend(bbox_to_anchor = (1.0, 0.5), loc='center left', prop={'size':10})
-		# plt.tight_layout()
+    # plt.tight_layout()
 
 
 
-	def readData(self):
-		'''read data from Labjack and save the most recent values'''
+    def readData(self):
+        '''read data from Labjack and save the most recent values'''
 
 
 
-		# pressure1 = random.random()
-		# pressure2 = random.random()
+        # pressure1 = random.random()
+        # pressure2 = random.random()
 
-		# gas1 = random.random()
-		# gas2 = random.random()
-		# gas3 = random.random()
+        # gas1 = random.random()
+        # gas2 = random.random()
+        # gas3 = random.random()
 
-		# force1 = random.random()
-		# force2 = random.random()
-		# force3 = random.random()
-
-
-		# The load cell is rated for 300lb with an output of 2mV / V
-		# We must first calibrate the cells to read 0 when the engine
-		# is attached but not firing
-		# offset = ???
-		# gain =
-		# scale = .002
-		# nominal = 300
-
-		force1 = self.d.getAIN(0)
-		force2 = self.d.getAIN(1)
-		force3 = self.d.getAIN(2)
-
-		pressure1 = self.d.getAIN(4)
-		# pressure2 = self.d.getAIN(4)
-		#
-		# gas1 = self.d.getAIN(5)
-		# gas2 = self.d.getAIN(6)
-		# gas3 = self.d.getAIN(7)
+        # force1 = random.random()
+        # force2 = random.random()
+        # force3 = random.random()
 
 
+        # The load cell is rated for 300lb with an output of 2mV / V
+        # We must first calibrate the cells to read 0 when the engine
+        # is attached but not firing
+        # offset = ???
+        # gain =
+        # scale = .002
+        # nominal = 300
 
-		# sample = [pressure1, pressure2, gas1, gas2, gas3, force1, force2, force3]
-		sample = [pressure1, force1, force2, force3]
+        force1 = self.d.getAIN(0)
+        force2 = self.d.getAIN(1)
+        force3 = self.d.getAIN(2)
 
-		logString = ','.join(map(str, sample))
-		self.log.write(logString+'\n')
-
-
-		self.pressure1 = self.pressure1[1:] + [pressure1]
-		# self.pressure2 = self.pressure2[1:] + [pressure2]
-		#
-		# self.gas1 = self.gas1[1:] + [gas1]
-		# self.gas2 = self.gas2[1:] + [gas2]
-		# self.gas3 = self.gas3[1:] + [gas3]
-
-		self.force1 = self.force1[1:] + [force1]
-		self.force2 = self.force2[1:] + [force2]
-		self.force3 = self.force3[1:] + [force3]
-
-
-	def init(self):
-		'''Function for initializing the graph. Called by FuncAnimation'''
-
-		y = [0] * self.length
-
-		self.pressureLine1.set_data(self.t, y)
-		# self.pressureLine2.set_data(self.t, y)
-		#
-		# self.gasLine1.set_data(self.t, y)
-		# self.gasLine2.set_data(self.t, y)
-		# self.gasLine3.set_data(self.t, y)
-
-		self.forceLine1.set_data(self.t, y)
-		self.forceLine2.set_data(self.t, y)
-		self.forceLine3.set_data(self.t, y)
-
-		# return [self.pressureLine1, self.pressureLine2, self.gasLine1, \
-		# 		self.gasLine2, self.gasLine3, \
-		# 		self.forceLine1, self.forceLine2, self.forceLine3]
-
-		return [self.pressureLine1, self.forceLine1, self.forceLine2, self.forceLine3]
+        pressure1 = self.d.getAIN(4)
+        # pressure2 = self.d.getAIN(4)
+        #
+        # gas1 = self.d.getAIN(5)
+        # gas2 = self.d.getAIN(6)
+        # gas3 = self.d.getAIN(7)
 
 
 
-	def animate(self, i):
-		'''Updates the graph. i is the frame number, which is ignored'''
-		now = time.time()
-		# print 'dt:', now - self.time
-		self.time = now
+        # sample = [pressure1, pressure2, gas1, gas2, gas3, force1, force2, force3]
+        sample = [pressure1, force1, force2, force3]
 
-		self.readData()
+        logString = ','.join(map(str, sample))
+        self.log.write(logString + '\n')
 
-		self.pressureLine1.set_ydata(self.pressure1)
-		# self.pressureLine2.set_ydata(self.pressure2)
-		#
-		# self.gasLine1.set_ydata(self.gas1)
-		# self.gasLine2.set_ydata(self.gas2)
-		# self.gasLine3.set_ydata(self.gas3)
+        self.pressure1 = self.pressure1[1:] + [pressure1]
+        # self.pressure2 = self.pressure2[1:] + [pressure2]
+        #
+        # self.gas1 = self.gas1[1:] + [gas1]
+        # self.gas2 = self.gas2[1:] + [gas2]
+        # self.gas3 = self.gas3[1:] + [gas3]
 
-		self.forceLine1.set_ydata(self.force1)
-		self.forceLine2.set_ydata(self.force2)
-		self.forceLine3.set_ydata(self.force3)
-
-		# return [self.pressureLine1, self.pressureLine2, self.gasLine1, \
-		# 		self.gasLine2, self.gasLine3, \
-		# 		self.forceLine1, self.forceLine2, self.forceLine3]
-
-		return [self.pressureLine1, self.forceLine1, self.forceLine2, self.forceLine3]
+        self.force1 = self.force1[1:] + [force1]
+        self.force2 = self.force2[1:] + [force2]
+        self.force3 = self.force3[1:] + [force3]
 
 
+    def init(self):
+        '''Function for initializing the graph. Called by FuncAnimation'''
 
-	def show(self):
-		ani = animation.FuncAnimation(self.fig, self.animate, frames=1, interval=self.interval, blit=True, init_func=self.init)
-		plt.show()
+        y = [0] * self.length
+
+        self.pressureLine1.set_data(self.t, y)
+        # self.pressureLine2.set_data(self.t, y)
+        #
+        # self.gasLine1.set_data(self.t, y)
+        # self.gasLine2.set_data(self.t, y)
+        # self.gasLine3.set_data(self.t, y)
+
+        self.forceLine1.set_data(self.t, y)
+        self.forceLine2.set_data(self.t, y)
+        self.forceLine3.set_data(self.t, y)
+
+        # return [self.pressureLine1, self.pressureLine2, self.gasLine1, \
+        # self.gasLine2, self.gasLine3, \
+        # 		self.forceLine1, self.forceLine2, self.forceLine3]
+
+        return [self.pressureLine1, self.forceLine1, self.forceLine2, self.forceLine3]
+
+
+    def animate(self, i):
+        '''Updates the graph. i is the frame number, which is ignored'''
+        now = time.time()
+        # print 'dt:', now - self.time
+        self.time = now
+
+        self.readData()
+
+        self.pressureLine1.set_ydata(self.pressure1)
+        # self.pressureLine2.set_ydata(self.pressure2)
+        #
+        # self.gasLine1.set_ydata(self.gas1)
+        # self.gasLine2.set_ydata(self.gas2)
+        # self.gasLine3.set_ydata(self.gas3)
+
+        self.forceLine1.set_ydata(self.force1)
+        self.forceLine2.set_ydata(self.force2)
+        self.forceLine3.set_ydata(self.force3)
+
+        # return [self.pressureLine1, self.pressureLine2, self.gasLine1, \
+        # self.gasLine2, self.gasLine3, \
+        # 		self.forceLine1, self.forceLine2, self.forceLine3]
+
+        return [self.pressureLine1, self.forceLine1, self.forceLine2, self.forceLine3]
+
+
+    def show(self):
+        ani = animation.FuncAnimation(self.fig, self.animate, frames=1, interval=self.interval, blit=True,
+                                      init_func=self.init)
+        plt.show()
+
 
 if __name__ == '__main__':
-	g = Graph()
-	g.show()
+    g = Graph()
+    g.show()
---
+++ websocket.py
@@ -0,0 +1,119 @@
+import socket, hashlib, base64, threading
+
+class PyWSock:
+    MAGIC = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+    HSHAKE_RESP = 'HTTP/1.1 101 Switching Protocols\r\n' + \
+                  'Upgrade: websocket\r\n' + \
+                  'Connection: Upgrade\r\n' + \
+                  'Sec-WebSocket-Accept: %s\r\n' + \
+                  '\r\n'
+    LOCK = threading.Lock()
+
+    clients = []
+
+    def __init__(self, port):
+        s = socket.socket()
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('', port))
+        s.listen(5)
+        while 1:
+            print ('Waiting for connection...')
+            connection, address = s.accept()
+            print ('Connection from: ' + str(address))
+            threading.Thread(target=self.handle_client, args=(connection, address)).start()
+            self.LOCK.acquire()
+            self.clients.append(connection)
+            self.LOCK.release()
+
+    def handle_client(self, client, address):
+        print('Handshaking...')
+        self.handshake(client)
+        print('Handshake successful...')
+        self.broadcast_resp('hello')
+
+        try:
+            while 1:
+                data = self.receive_data(client)
+                print("received [%s]" % (data,))
+                self.broadcast_resp(data)
+        except Exception as e:
+            print("Exception %s" % (str(e)))
+
+        print('Client closed: ' + str(address))
+
+        self.LOCK.acquire()
+        self.clients.remove(client)
+        self.LOCK.release()
+
+        client.close()
+
+    def handshake(self, client):
+        data = client.recv(2048)
+
+        headers = {}
+        lines = data.splitlines()
+        for l in lines:
+            parts = l.split(": ", 1)
+            if len(parts) == 2:
+                headers[parts[0]] = parts[1]
+        headers['code'] = lines[len(lines) - 1]
+
+        key = headers['Sec-WebSocket-Key']
+        resp_data = self.HSHAKE_RESP % (base64.b64encode(hashlib.sha1(key + self.MAGIC).digest()),)
+
+        return client.send(resp_data)
+
+    def receive_data(self, client):
+        # as a simple server, we expect to receive:
+        # - all data at one go and one frame
+        #    - one frame at a time
+        #    - text protocol
+        #    - no ping pong messages
+        data = bytearray(client.recv(512))
+        if (len(data) < 6):
+            raise Exception("Error reading data")
+        # FIN bit must be set to indicate end of frame
+        assert (0x1 == (0xFF & data[0]) >> 7)
+        # data must be a text frame
+        # 0x8 (close connection) is handled with assertion failure
+        assert (0x1 == (0xF & data[0]))
+        # assert that data is masked
+        assert (0x1 == (0xFF & data[1]) >> 7)
+        datalen = (0x7F & data[1])
+
+        #print("received data len %d" %(datalen,))
+
+        str_data = ''
+        if datalen > 0:
+            mask_key = data[2:6]
+            masked_data = data[6:(6 + datalen)]
+            unmasked_data = [masked_data[i] ^ mask_key[i % 4] for i in range(len(masked_data))]
+            str_data = str(bytearray(unmasked_data))
+        return str_data
+
+    def broadcast_resp(self, data):
+        # 1st byte: fin bit set. text frame bits set.
+        # 2nd byte: no mask. length set in 1 byte. 
+        resp = bytearray([0b10000001, len(data)])
+        # append the data bytes
+        for d in bytearray(data):
+            resp.append(d)
+
+        self.LOCK.acquire()
+        for client in self.clients:
+            try:
+                client.send(resp)
+            except:
+                print("error sending to a client")
+        self.LOCK.release()
+
+    def close(self):
+
+        self.LOCK.acquire()
+        for client in self.clients:
+            client.close()
+            self.clients.remove(client)
+        self.LOCK.release()
+
+
+ws = PyWSock(2005)
