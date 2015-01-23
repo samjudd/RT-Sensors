c7b757a302deee8311653ee94e5074ae7a5464af
mvernacc <mvernacc@mit.edu>
2015-01-22 19:57:01 -0500

    start sensor object

---
+++ LabJackSensor.py
@@ -0,0 +1,57 @@
+from sensor import Sensor
+from LabJackPython import LabJackException
+
+class LabJackSensor(Sensor):
+	def __init__(self, lab_jack, pin):
+		""" Create a new LabJackSensor.
+		:param lab_jack: Lab Jack Analog to Digital converter object, see http://labjack.com/support/labjackpython.
+		:type lab_jack: u3
+		:param pin: The analog input pin on the LabJack which the sensor's signal is connected to.
+		:type pin: integer >= 0
+		"""
+		self.lab_jack = lab_jack
+		if pin < 0:
+			raise ValueError('LabJack analog input pin for sensor must be nonnegative')
+		self.pin = pin
+		super(LabJackSensor, self).__init__()
+
+	def read_raw(self):
+		""" Read a new raw value from the sensor hardware interface. 
+		Overrides fucntion in Sensor. """
+		try:
+			self.latest_raw_value = self.LabJack.getAIN(self.pin)
+		except LabJackException, e:
+			print 'LabJackException during sensor read', e
+			self.valid_satus = False
+		return self.latest_raw_value
+
+class PressureSensor(LabJackSensor):
+	def __init__(self, lab_jack, pin, name, offset, scale):
+		""" Create a new pressure sensor.
+		:param lab_jack: Lab Jack Analog to Digital converter object, see http://labjack.com/support/labjackpython.
+		:type lab_jack: u3
+		:param pin: The analog input pin on the LabJack which the sensor's signal is connected to.
+		:type pin: integer >= 0
+		:param name: The sensor's name (e.g. 'chamber pressure').
+		:param offset: The sensor's raw output at atmospheric pressure [volt].
+		:param scale: The sensor's voltage-to-pressure scaling [pascal volt^-1].
+		"""
+		self.name = name
+		self.type = 'pressure'
+		self.offset = offset
+		self.scale = scale
+		super(PressureSensor, self).__init__(lab_jack, pin)
+
+	def convert_raw(self, raw):
+		""" Convert a raw sensor reading (i.e. volts) to physical units.
+		Overrides function in Sensor.
+		:param raw: Raw sensor reading [volt].
+		:returns: Sensed pressure [pascal].
+		"""
+		pressure = (raw - self.offset) * self.scale
+		# If the sensed pressure is less than vacuum, then the reading is invalid.
+		# If the pressure transducers are not getting power, they will read ~0 volts and produce sub-vacuum
+		# pressure readings.
+		if pressure < -103e5:
+			self.valid_satus = False
+			print 'Warning: sub-vacuum pressure reading. Are the pressure transducers powered?'
---
+++ sensor.py
@@ -0,0 +1,30 @@
+class Sensor:
+	def __init__(self):
+		# The latest raw readign from the sensor
+		self.latest_raw_value = None
+		# The name of the sensor channel (e.g. 'Chamber pressure')
+		self.name = ''
+		# The type of sensor (e.g. 'pressure', 'force', 'temperature')
+		self.type = ''
+		# The physical units of the sensor's raw reading (e.g. 'volts')
+		self.raw_units = ''
+		# The physical units of the sensor's converted reading (e.g. 'pascal', 'newton')
+		self.converted_units = ''
+		# Senor status. True if the sensor is functioning properly and reading valid values.
+		self.valid_status = False
+
+	def read_raw(self):
+		""" Read a new raw value from the sensor hardware interface. """
+		pass
+
+	def convert_raw(self, raw):
+		""" Convert a raw sensor reading (i.e. volts) to physical units. """
+		pass
+
+	def get_raw(self):
+		""" Get the latest raw value (without reading from the hardware). """
+		return self.latest_raw_value
+
+	def get_converted(self):
+		""" Get the latest converted (i.e. physical units) value (without reading from the hardware). """
+		return self.convert_raw(self.latest_value)
