96ad0c5de54a91744bcbe08d66fde9571cbf3b98
mvernacc <mvernacc@mit.edu>
2015-01-25 13:09:29 -0500

    switch liveGraph to sensor objects
    liveGraph now plots from a list of pressure sensors objects, rather than
    reading and converting values from the LabJack directly. I also made
    liveGraph use fake_labjack so i could test without the real LabJack
    hardware.

--- liveGraph.py
+++ liveGraph.py
@@ -8,8 +8,9 @@ import matplotlib.animation as animation
 import random
 import datetime
 import time
-import u3
+from fake_labjack import u3
 from thermocouple_convert import ThermocoupleConverter
+from LabJackSensor import PressureSensor
 #import threading #use to make function loop every x seconds 
 
 class Graph:
@@ -70,33 +71,47 @@ class Graph:
         self.ax3.set_ylabel('Temperature (deg C)')
 	
 	#set the limits of the axes, this needs to change
-        self.ax1.set_ylim([0, 5])
+        self.ax1.set_ylim([0, 10e6])
         self.ax2.set_ylim([-5, 5])
         self.ax3.set_ylim([0,2000])
 
 	#array of zeros to hold points
         y = [0] * self.length
 
-	#make pressure lines on plots
-        self.pressureLine1 = self.ax1.plot(self.t, y, color='orange', label="Fuel Pressure")[0]
-        self.pressureLine2 = self.ax1.plot(self.t, y, color='green', label="Ox Pressure")[0]
-        self.pressureLine3 = self.ax1.plot(self.t, y, color='red', label="Chmb Pressure")[0]
+	# #make pressure lines on plots
+ #        self.pressureLine1 = self.ax1.plot(self.t, y, color='orange', label="Fuel Pressure")[0]
+ #        self.pressureLine2 = self.ax1.plot(self.t, y, color='green', label="Ox Pressure")[0]
+ #        self.pressureLine3 = self.ax1.plot(self.t, y, color='red', label="Chmb Pressure")[0]
 	
-	#make force lines on plots
-        self.forceLine1 = self.ax2.plot(self.t, y, label="Force 1")[0]
-        self.forceLine2 = self.ax2.plot(self.t, y, label="Force 2")[0]
-        self.forceLine3 = self.ax2.plot(self.t, y, label="Force 3")[0]
+	# #make force lines on plots
+ #        self.forceLine1 = self.ax2.plot(self.t, y, label="Force 1")[0]
+ #        self.forceLine2 = self.ax2.plot(self.t, y, label="Force 2")[0]
+ #        self.forceLine3 = self.ax2.plot(self.t, y, label="Force 3")[0]
         
-        #make temperature line on plot
-        self.tempLine1 = self.ax3.plot(self.t, y, label='Temp 1')[0]
+ #        #make temperature line on plot
+ #        self.tempLine1 = self.ax3.plot(self.t, y, label='Temp 1')[0]
 
-	#make the legend look nice	
+	
+        # Temperature data conversion
+        # self.tc = ThermocoupleConverter('thermocouple_type_B.csv')
+
+        self.sensors = []
+        pins = [4,5,6]
+        names = ['Chamber', 'Ox', 'Fuel']
+        colors = ['orange', 'green', 'red']
+        for pin, name, color in zip(pins, names, colors):
+            ps = PressureSensor(self.d, pin, name, 1.0, 34.47e6/4.0)
+            # line and record are tecked on here, they are not typically sensor
+            # class attributes.
+            ps.line = self.ax1.plot(self.t, y, color=color, label=name)[0]
+            ps.record = [0] * self.length
+            self.sensors.append(ps)
+        
+        #make the legend look nice  
         self.ax1.legend(bbox_to_anchor=(1.0, 0.5), loc='center left', prop={'size': 10})
-        self.ax2.legend(bbox_to_anchor=(1.0, 0.5), loc='center left', prop={'size': 10})
-        self.ax3.legend(bbox_to_anchor=(1.0, 0.5), loc='center left', prop={'size': 10})
+        # self.ax2.legend(bbox_to_anchor=(1.0, 0.5), loc='center left', prop={'size': 10})
+        # self.ax3.legend(bbox_to_anchor=(1.0, 0.5), loc='center left', prop={'size': 10})
 
-    # Temperature data conversion
-        self.tc = ThermocoupleConverter('thermocouple_type_B.csv')
 
     # Start time
         self.start_time = datetime.datetime.now()
@@ -104,122 +119,37 @@ class Graph:
     def readData(self):
         '''read data from Labjack and save the most recent values'''
 
-        # The load cell is rated for 300lb with an output of 2mV / V
-        # We must first calibrate the cells to read 0 when the engine
-        # is attached but not firing
-        # offset = ???
-        # gain =
-        # scale = .002
-        # nominal = 300
-
         # sample time
         sample_time =(datetime.datetime.now() - self.start_time).total_seconds()
 
-	    #get force readings (in volts) from labjack
-        # force1_v = self.d.getAIN(self.Force1pin)
-        # force2_v = self.d.getAIN(self.Force2pin)
-        # force3_v = self.d.getAIN(self.Force3pin)
-
-	     #get pressure readings (in volts) from labjack
-        pressure1_v = self.d.getAIN(self.Pressure1pin)
-        pressure2_v = self.d.getAIN(self.Pressure2pin)
-        pressure3_v = self.d.getAIN(self.Pressure3pin)
-        
-        #get temperature readings (in volts) from labjack
-        n = 1
-        temp1_v = 0
-        for i in xrange(n):
-            temp1_v += self.d.getAIN(self.Temp1pin)
-        temp1_v = temp1_v / float(n)  
-        
-        #using calibrations from earlier, convert the voltage readings to meaningful data 
-        #JOHANNES REPLACE THE 1s with actual things
-        force1 = 0 #force1_v * 1
-        force2 = 0 #force2_v * 1
-        force3 = 0 #force3_v * 1
-        
-        pressure1 = pressure1_v * 1
-        pressure2 = pressure2_v * 1
-        pressure3 = pressure3_v * 1
-        
-        # if temp1_v < 0:
-        #     temp1_v = 0
-
-        #temp_diff_v = temp1_v - force1_v
-        # temp_diff_v = temp1_v
-
-        temp_amplifier_gain = 5 + (80.0e3 / 194)
-        temp_amplifier_offset = 0
-
-        temp1 = 0
-        try:
-            temp1 = self.tc.get_temperature((temp1_v - temp_amplifier_offset) / temp_amplifier_gain)
-        except ValueError:
-            pass
-        #print 'temp1_v = %.3f volts amplified, %.1f K' % (temp1_v, temp1)
-        print 'pressure 3 = %.3f volts' % (pressure3_v)
-        sample = [sample_time, force1, force2, force3, pressure1, pressure2, pressure3, temp1]
-
-        #write sample to log
-        logString = ','.join(map(str, sample))
-        self.log.write(logString + '\n')
-        
-        # self.force1 = self.force1[1:] + [force1]
-        # self.force2 = self.force2[1:] + [force2]
-        # self.force3 = self.force3[1:] + [force3]
-
-        self.pressure1 = self.pressure1[1:] + [pressure1]
-        self.pressure2 = self.pressure2[1:] + [pressure2]
-        self.pressure3 = self.pressure3[1:] + [pressure3]
-        
-        self.temp1 = self.temp1[1:] + [temp1]
+        for sensor in self.sensors:
+            new_value = sensor.convert_raw(sensor.read_raw())
+            sensor.record = sensor.record[1:] + [new_value]       
 
 
     def init(self):
         '''Function for initializing the graph. Called by FuncAnimation'''
 
         y = [0] * self.length
-	
-	#initializes the graph to the inital values
-	    # self.forceLine1.set_data(self.t, y)
-     #    self.forceLine2.set_data(self.t, y)
-     #    self.forceLine3.set_data(self.t, y)
-	
-        self.pressureLine1.set_data(self.t, y)
-        self.pressureLine2.set_data(self.t, y)
-        self.pressureLine3.set_data(self.t, y)
-        
-        # self.tempLine1.set_data(self.t, y)
+        lines = []
+        for sensor in self.sensors:
+            sensor.line.set_data(self.t, y)
+            lines.append(sensor.line)
 
-        return [self.forceLine1, self.forceLine2, self.forceLine3, self.pressureLine1, self.pressureLine2, self.pressureLine3, self.tempLine1]
-
+        return lines
 
     def animate(self, i):
         '''Updates the graph. i is the frame number, which is ignored'''
         self.time = time.time()
 
-        # read data multiple times fat each animation step for a higher data rate
-        for i in xrange(10):
-            # try:
-            self.readData()
-            # except:
-            #     print 'caught LabJackException'
+        self.readData()
 
-	    #plots the current data
+        lines = []
+        for sensor in self.sensors:
+            sensor.line.set_ydata(sensor.record)
+            lines.append(sensor.line)
 
-        self.forceLine1.set_ydata(self.force1)
-        self.forceLine2.set_ydata(self.force2)
-        self.forceLine3.set_ydata(self.force3)
-        
-        self.pressureLine1.set_ydata(self.pressure1)
-        self.pressureLine2.set_ydata(self.pressure2)
-        self.pressureLine3.set_ydata(self.pressure3)
-
-        
-        self.tempLine1.set_ydata(self.temp1)
-
-        return [self.forceLine1, self.forceLine2, self.forceLine3, self.pressureLine1, self.pressureLine2, self.pressureLine3, self.tempLine1]
-
+        return lines
 
     def show(self):
         ani = animation.FuncAnimation(self.fig, self.animate, frames=1, interval=self.interval, blit=True,
